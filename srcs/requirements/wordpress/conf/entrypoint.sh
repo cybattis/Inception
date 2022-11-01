336689939c18009db8eac1610bfdb31923f8c634
cybattis <cybattis@student.42lyon.fr>
2022-11-01 11:06:04 +0100

    update

--- srcs/requirements/wordpress/conf/entrypoint.sh
+++ srcs/requirements/wordpress/conf/entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if ! wp core is-installed --quiet --allow-root; then
+if ! /var/www/wordpress/wp-config.php; then
     echo "Installing wordpress"
-    wp core download --allow-root
+    wp core --allow-root download --locale=fr_FR --force 
     wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST:$MYSQL_PORT --prompt=$MYSQL_PASSWORD
-    wp core install --allow-root --url=$DOMAIN_NAME --title=$WP_NAME --admin_user=$WORDPRESS_ADMIN --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=cybattis@student.42lyon.fr
+    wp core install --allow-root --url=$DOMAIN_NAME --title=$WP_NAME --admin_user=$WORDPRESS_ADMIN --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=cybattis@student.42lyon.fr --skip-email
     wp user create $WORDPRESS_USER "$WORDPRESS_USER"@randomuser.com --role='subscriber' --user_pass="$WORDPRESS_PASSWORD" --allow-root
     wp option update comment_registration 1 --allow-root
+
+    wp theme install --allow-root dark-mode --activate
 
     chown www-data:www-data -R /var/www/wordpress
     chmod -R 755 /var/www/wordpress
@@ -14,4 +16,4 @@ else
     echo "Wordpress already install"
 fi
 
-exec /usr/sbin/php-fpm7.3 -FR
+php-fpm7.3 --nodaemonize
