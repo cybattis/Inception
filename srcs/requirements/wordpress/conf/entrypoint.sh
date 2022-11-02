234cb5e4f833071a09b39f7d1df18ad9b4457695
cybattis <cybattis@student.42lyon.fr>
2022-11-02 16:54:35 +0100

    update dockerfile add conf file Still error 404

--- srcs/requirements/wordpress/conf/entrypoint.sh
+++ srcs/requirements/wordpress/conf/entrypoint.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 if [ ! -f /var/www/wordpress/wp-config.php ]; then
+
+    while ! mariadb --host=${MYSQL_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} ${MYSQL_DATABASE} &> /dev/null; do
+            echo "Waiting on mariadb config"
+            sleep 2
+        done
+
     echo "Installing wordpress"
-    cd /var/www/wordpress
-    wp core download --allow-root --force 
     wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST:$MYSQL_PORT --prompt=$MYSQL_PASSWORD
     wp core install --allow-root --url=$DOMAIN_NAME --title=$WP_NAME --admin_user=$WORDPRESS_ADMIN --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=cybattis@student.42lyon.fr --skip-email
-  
     wp user create --allow-root $WORDPRESS_USER "$WORDPRESS_USER"@randomuser.com --role='subscriber' --user_pass="$WORDPRESS_PASSWORD"
-   
+    wp theme install twentytwenty --activate --allow-root
     # wp option update comment_registration 1 --allow-root
-    # wp theme install --allow-root dark-mode --activate
 
     echo "Wordpress installed"
 else
