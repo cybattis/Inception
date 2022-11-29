57773968e933de05d5b0de787bada2f7708fa72d
cybattis <cybattis@student.42lyon.fr>
2022-11-29 15:17:39 +0100

    fix error message when init mariadb

--- srcs/requirements/mariadb/conf/entrypoint.sh
+++ srcs/requirements/mariadb/conf/entrypoint.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-service mysql start
 
-if mysql "${MYSQL_DATABASE}" >/dev/null 2>&1 </dev/null
-then
-    echo "-- Create Database"
+if [ ! -d /var/lib/mysql/${MYSQL_DATABASE} ]; then
+    service mysql start
     mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
     mysql -e "USE ${MYSQL_DATABASE};"
-
-    echo "-- Create User"
     mysql -e "CREATE USER IF NOT EXISTS ${MYSQL_USER}@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
     mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
     mysql -e "FLUSH PRIVILEGES;"
+    sleep 3
+    service mysql stop
     echo "Database setup"
 else
     echo "Database already setup"
